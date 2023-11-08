cc3113e6d39da009ace0e1f9eee9e6da4eb571fd
j-kyung99 <apfwnls14@naver.com>
2023-11-08 15:02:49 +0900

    #2.3 Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9r3JeH0DZNWymK9j4Dog2ZNu73kI1VjE",
+  authDomain: "nwitter-reloaded-cbcc8.firebaseapp.com",
+  projectId: "nwitter-reloaded-cbcc8",
+  storageBucket: "nwitter-reloaded-cbcc8.appspot.com",
+  messagingSenderId: "304945084345",
+  appId: "1:304945084345:web:8542cb4fe89ec82cd86868",
+};
+
+const app = initializeApp(firebaseConfig);
